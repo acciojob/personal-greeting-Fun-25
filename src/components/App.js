fc088f71efae2191730bd50bd3000f130647886a
Fun-25 <muskandodmani760@gmail.com>
2023-06-26 20:11:35 +0530

    submitting code-Mon Jun 26 2023 20:11:30 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,18 @@ const App = () => {
   let [name,setName]=useState("")
   return (
     <div>
-         <form>
-           <p>Enter your name:</p>
-           <input type="text" onChange={(e)=>{setName(e.target.value)}}/> 
-         </form>
-         <h2>Hello {name}!</h2>
+      <form>
+        <p>Enter your name:</p>
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </form>
+      {name && <h2>Hello {name}!</h2>}
     </div>
-  )
+  );
 }
 
 export default App
